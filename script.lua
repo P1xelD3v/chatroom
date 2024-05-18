e2946a1beac1214fd97dee5937f54128cb53d217
MrPixel <124072273+PixelFacts@users.noreply.github.com>
2024-05-18 11:13:52 +0200

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,1 +1,15 @@
-get("message").set_content("test_")
+local API_URL = "https://script.google.com/macros/s/AKfycbxo8V6upSRXkJQpAwx5J32Ty15kYRSayr5yP7Aqo0FXJDtHw7tIiI1exx4Bnr9ktdsM/exec"
+
+get("message").set_content("test")
+
+function sendPostRequest(method)
+    local res = fetch({
+		url = API_URL,
+		method = method,
+		headers = { ["Content-Type"] = "application/json" },
+	})
+
+    return res
+end
+
+get("message").set_content(sendPostRequest("/send-message"))
