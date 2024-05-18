1d1bfedf102fbf15abf70504755d4e2e8357aa77
MrPixel <124072273+PixelFacts@users.noreply.github.com>
2024-05-18 11:35:39 +0200

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local API_URL = "https://script.google.com/macros/s/AKfycbxo8V6upSRXkJQpAwx5J32Ty15kYRSayr5yP7Aqo0FXJDtHw7tIiI1exx4Bnr9ktdsM/exec"
 
-get("message").set_content("Default Text")
+get("message").set_content("Default Text Test")
 
 function sendPostRequest(method)
     local res = fetch({
@@ -13,3 +13,4 @@ function sendPostRequest(method)
 end
 
 get("message").set_content(sendPostRequest("GET"))
+get("message").set_content("non default text")
